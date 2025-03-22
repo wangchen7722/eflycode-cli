ffddf2ea0848d5326315527866f016fb7f301b9a
WangChen <dawnwccc712@gmail.com>
2025-03-23 02:18:57 +0800

    refactor(test.py): 使用环境变量配置LLMConfig
    将硬编码的LLMConfig配置替换为从环境变量读取，提高代码的灵活性和安全性

--- test.py
+++ test.py
@@ -5,18 +5,13 @@ from echo.llms.llm_engine import LLMConfig
 from echo.llms.openai_engine import OpenAIEngine
 from echo.tools import ReadFileTool, CreateFileTool, UpdateFileTool, SearchFilesTool, ListFilesTool, ExecuteCommandTool, ListCodeDefinitionsTool
 
-# llm_config = LLMConfig(
-#     model=os.environ["ECHO_MODEL"],
-#     base_url=os.environ["ECHO_BASE_URL"],
-#     api_key=os.environ["ECHO_API_KEY"],
-#     temperature=0.1
-# )
 llm_config = LLMConfig(
-    model="ep-20250220154917-m5tv5",
-    base_url="https://ark.cn-beijing.volces.com/api/v3",
-    api_key="cb7cb751-3de9-4e2b-89bb-bbd7a303f193",
+    model=os.environ["ECHO_MODEL"],
+    base_url=os.environ["ECHO_BASE_URL"],
+    api_key=os.environ["ECHO_API_KEY"],
     temperature=0.1
 )
+
 developer = Developer(
     name="developer",
     llm_engine=OpenAIEngine(llm_config),

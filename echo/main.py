1bb71c0296d3114f422fad6ef04805f930133f9d
WangChen <dawnwccc712@gmail.com>
2025-03-25 01:10:30 +0800

    refactor: 更新LLMConfig中的模型配置
    将默认模型从ep-20250220155009-8ckjl切换回ep-20250220154917-m5tv5，以确保使用更稳定的模型版本

--- echo/main.py
+++ echo/main.py
@@ -6,8 +6,8 @@ from echo.tools import ReadFileTool, CreateFileTool, EditFileTool, InsertFileToo
 
 def main():
     llm_config = LLMConfig(
-        # model="ep-20250220154917-m5tv5",
-        model="ep-20250220155009-8ckjl",
+        model="ep-20250220154917-m5tv5",
+        # model="ep-20250220155009-8ckjl",
         base_url="https://ark.cn-beijing.volces.com/api/v3",
         api_key="cb7cb751-3de9-4e2b-89bb-bbd7a303f193",
         temperature=0.1
